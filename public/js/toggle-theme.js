5d6d5576403a26cf513a4ecb05f1b990319ddbaf
Zach Sousa <zs20np@snpsteam.com>
2024-05-02 13:42:28 -0400

    localStorage usage

--- public/js/toggle-theme.js
+++ public/js/toggle-theme.js
@@ -1,11 +1,29 @@
-console.log("loaded");
-
+/**
+ * this function checks the current href of the css, and stores the new 
+ * one in the localStorage
+ */
 function toggleTheme() { 
-    var theme = document.querySelector("#css");
-
-    if (theme.getAttribute('href') == './public/css/lightstyle.css') { 
-        theme.setAttribute('href', './public/css/darkstyle.css'); 
+    var themeStyling = document.querySelector("#css");
+    if (window.localStorage.getItem("theme") == './public/css/lightstyle.css') { 
+        window.localStorage.setItem("theme", "./public/css/darkstyle.css");
     } else { 
-        theme.setAttribute('href', './public/css/lightstyle.css'); 
+        window.localStorage.setItem("theme", "./public/css/lightstyle.css");
     } 
-} 
+    getTheme();
+}
+
+/**
+ * this function is used to retrieve the current theme from the
+ * localStorage
+ */
+function getTheme() {
+    var themeStyling = document.querySelector("#css");
+    themeStyling.setAttribute('href', window.localStorage.getItem("theme"));
+}
+
+/**
+ * call the getTheme function on page load
+ */
+window.addEventListener("load", (event) => {
+    getTheme();
+});
